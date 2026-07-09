c027f1115d9d64bce92f44b5fb4dbfaee1b40f42
Ananya Jain <ananyaj1515@gmail.com>
2026-07-09 13:31:40 +0800

    Initalise project and define data model

---
+++ app/models/models.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from typing import List, Optional
+from sqlmodel import Column, Field, SQLModel
+from sqlalchemy.dialects.postgresql import ARRAY, TEXT
+
+class Reviews(SQLModel, table=True):
+    id: int = Field(primary_key=True)
+    course_code: str = Field(max_length=10, nullable=False)
+    raw_message: str = Field(nullable=False)
+    created_at: datetime = Field(nullable=False)
+    disqus_id: int = Field(nullable=False, unique=True)
+    likes: int = Field(default=0, nullable=False)
+    parent_disqus_id: int = Field(nullable=True)
+
+class Summary(SQLModel, table=True):
+    id: int = Field(primary_key=True)
+    course_code: str = Field(max_length=10, nullable=False)
+    summary: str = Field(nullable=False)
+    created_at: datetime = Field(default_factory=datetime.now, nullable=False)
+    praises: List[str] = Field(default=[], sa_column=Column(ARRAY(TEXT)))
+    complaints: List[str] = Field(default=[], sa_column=Column(ARRAY(TEXT)))
+    difficulty: Optional[float] = Field(maximum=10, nullable=True)
+    workload: Optional[float] = Field(maximum=10, nullable=True)
+    enjoyability: Optional[float] = Field(maximum=10, nullable=True)
